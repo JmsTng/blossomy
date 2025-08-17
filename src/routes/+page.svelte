571c5b73da82103b2dcbf5118e0babe1ba90f9a4
James Tung <james.ftung@gmail.com>
2025-08-16 23:00:59 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import Clock from "./Clock.svelte";
+  import Settings from "./Settings.svelte";
+
+  let hourFormat = $state("h12");
+</script>
+
+<main class="container">
+  <Clock {hourFormat}/>
+  <Settings bind:format={hourFormat}/>
+</main>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Bagel+Fat+One&display=swap');
+
+  :root {
+    font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+
+    color: #0f0f0f;
+    background-color: #f6f6f6;
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+  }
+
+  .container {
+    margin: 0;
+    padding-top: 10vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+  }
+
+  h1 {
+    font-family: "Bagel Fat One", system-ui;
+    font-weight: 400;
+    font-style: normal;
+
+    text-align: center;
+    font-size: 4em;
+
+    text-transform: uppercase;
+    /* -webkit-text-stroke: #0f0f0f 3px; */
+  }
+
+  @media (prefers-color-scheme: dark) {
+    :root {
+      color: #f6f6f6;
+      background-color: #2f2f2f;
+    }
+  }
+
+</style>
---
+++ src/routes/Clock.svelte
@@ -0,0 +1,60 @@
+<script>
+  let { hourFormat } = $props();
+  let date = new Intl.DateTimeFormat(undefined, {
+    hourCycle: hourFormat,
+    hour: "numeric",
+    minute: "numeric",
+  });
+  let time = $state("");
+
+  $effect(() => {
+    const interval = setInterval(() => {
+      time = date.format().replaceAll(".", "");
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+</script>
+
+<h1>{time}</h1>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Bagel+Fat+One&display=swap');
+
+  :root {
+    font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+
+    color: #0f0f0f;
+    background-color: #f6f6f6;
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+  }
+
+  h1 {
+    font-family: "Bagel Fat One", system-ui;
+    font-weight: 400;
+    font-style: normal;
+
+    text-align: center;
+    font-size: 4em;
+
+    text-transform: uppercase;
+    /* -webkit-text-stroke: #0f0f0f 3px; */
+  }
+
+  @media (prefers-color-scheme: dark) {
+    :root {
+      color: #f6f6f6;
+      background-color: #2f2f2f;
+    }
+  }
+</style>
